191b891ae2a0b981b2796944fb998923d9cfad54
junhee <86397805+jojunhui125@users.noreply.github.com>
2022-01-19 16:04:49 +0900

    Add files via upload

---
+++ Level 1 나누어 떨어지는 숫자 배열.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor) {
+    vector<int> answer;
+    int cnt=0;
+    
+    for(int i=0;i<arr.size();i++)
+    {
+        if(arr[i]%divisor==0)
+        {
+            answer.push_back(arr[i]);
+            cnt++;
+        }
+    }
+    
+    if(cnt==0)
+    {
+        answer.push_back(-1);
+    }
+    sort(answer.begin(),answer.end());
+    return answer;
+}

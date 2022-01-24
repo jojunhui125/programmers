93c0e055119156c5dbb3fa64f2331af99299a4fe
junhee <86397805+jojunhui125@users.noreply.github.com>
2022-01-24 19:27:47 +0900

    Add files via upload

---
+++ Level 1 부족한 금액 계산하기.cpp
@@ -0,0 +1,24 @@
+using namespace std;
+
+long long solution(int price, int money, int count)
+{
+    long long answer = 0;
+    int pos=0;
+    long long sum=0;
+    
+  
+    for(int i=1;i<=count;i++)
+    {
+        sum+=i*price;
+    }
+    answer=sum-money;
+    if(answer>0)
+    {
+        return answer;
+    }
+    else
+    {
+        return 0;
+    }
+
+}

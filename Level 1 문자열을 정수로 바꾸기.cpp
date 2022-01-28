4e4a8bf55b9d0bcd4cab64fba1e103afa9b82fbd
junhee <86397805+jojunhui125@users.noreply.github.com>
2022-01-28 14:19:48 +0900

    Add files via upload

---
+++ Level 1 문자열을 정수로 바꾸기.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+    int pos = 1;
+    for(int i=s.size();i>=0;i--)
+    {
+        if(s[i]>=48 && s[i]<=57)
+        {
+            answer+=(s[i]-'0')*pos;
+            pos*=10;
+        }
+        else if(s[i]==45)
+        {
+            answer*=-1;
+        }
+    }
+    return answer;
+}
